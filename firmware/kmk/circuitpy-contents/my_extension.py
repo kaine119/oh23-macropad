51a73726af0582c5682b46e20d5e74dcf4684788
kaine119 <9492636+kaine119@users.noreply.github.com>
2023-06-14 08:42:36 +0800

    Prep for workshop

--- firmware/kmk/circuitpy-contents/my_extension.py
+++ firmware/kmk/circuitpy-contents/my_extension.py
@@ -2,26 +2,12 @@
 # and do stuff based on values.
 
 from kmk.extensions import Extension
-import board
-import neopixel
-from adafruit_fancyled.adafruit_fancyled import CHSV
-import busio
-import math
+# Import stuff here
 
-import adafruit_vl53l0x
-
-pixels = neopixel.NeoPixel(board.GP12, 1)
-i2c = busio.I2C(board.GP7, board.GP6)
-sensor = adafruit_vl53l0x.VL53L0X(i2c)
 
 class MyExtension(Extension):
     def after_hid_send(self, keyboard):
-        # print(sensor.range)
-        brightness = 255 - math.floor(sensor.range / 300 * 255)
-        print(brightness)
-        colour = CHSV(0, 255, brightness)
-        pixels[0] = colour.pack()
-        pixels.show()
+        # TODO: hook into event loop here
         return
         
     def on_runtime_enable(self, keyboard):
@@ -34,9 +20,6 @@ class MyExtension(Extension):
         return
 
     def before_matrix_scan(self, keyboard):
-        '''
-        Return value will be injected as an extra matrix update
-        '''
         return
 
     def after_matrix_scan(self, keyboard):
